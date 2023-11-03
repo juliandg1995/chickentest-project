a13719a289c656850387deea36f5c5613eb08575
juliandg1995 <gomezjulian1995@gmail.com>
2023-11-03 14:31:02 -0300

    Merge con feature/buy_sell

--- chickentest-backend/src/main/java/com/retooling/chickentestbackend/controller/ChickenController.java
+++ chickentest-backend/src/main/java/com/retooling/chickentestbackend/controller/ChickenController.java
@@ -4,29 +4,17 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.retooling.chickentestbackend.model.Farm;
-import com.retooling.chickentestbackend.dto.ChickenRequestDTO;
-import com.retooling.chickentestbackend.dto.EggRequestDTO;
-import com.retooling.chickentestbackend.exceptions.farm.FarmNotFoundException;
 import com.retooling.chickentestbackend.model.Chicken;
-import com.retooling.chickentestbackend.model.Egg;
 import com.retooling.chickentestbackend.services.ChickenService;
 import com.retooling.chickentestbackend.services.FarmService;
 
 import jakarta.persistence.EntityNotFoundException;
-import jakarta.transaction.Transactional;
 
 @RestController
 @RequestMapping("/chickens")
@@ -35,6 +23,7 @@ public class ChickenController {
 	@Autowired
 	private ChickenService chickenService;
 	
+	@SuppressWarnings("unused")
 	private FarmService farmService;
 	
 //	@PostMapping(value = "/createChicken", produces = MediaType.APPLICATION_JSON_VALUE)
